d06dbf8e69e255d022b981a47c4c24e81e04049e
James Brown <Jamestron@gmail.com>
2014-03-14 23:05:12 +1300

    initial commit

---
+++ Assets/Scripts/player_Script.js
@@ -0,0 +1,51 @@
+﻿#pragma strict
+var leftStickX : float;
+var leftStickY : float;
+var idleCount : int;
+
+var state : int;
+
+function Start () {
+	state = 1;
+}
+
+function Update () {
+
+	if (state == 1){ //standing
+
+		if (Input.GetAxis("leftStickX")>0.2 || Input.GetAxis("leftStickX")<-0.2){
+				transform.Translate(Vector3(Input.GetAxis("leftStickX") * Time.deltaTime,0,0));
+		}
+		if (Input.GetAxis("leftStickY")>0.2){
+			state = 2; // change to crouch
+		}
+	}
+	if (state == 2){ //crouch
+		if (Input.GetAxis("leftStickY")<-0.2){
+			state = 3; // change to jump
+		}
+	}
+	if (state == 3){ //jump
+		transform.Translate(Vector3(0,2 * Time.deltaTime,0));
+		if (transform.position.y >= 3){
+			state = 4; // change to fall at the top of the jump
+		}
+	}
+	if (state == 4){ //falling
+		transform.Translate(Vector3(0,-2 * Time.deltaTime,0));
+		if (transform.position.y <= 0.5){
+			state = 1; // change to stand at the bottom of the fall
+		}
+	}
+		
+
+}
+
+function checkIdle(){
+	if (Input.GetAxis("leftStickY")>-0.2 && Input.GetAxis("leftStickY")<0.2 ){
+		yield WaitForSeconds(0.1);
+			if (Input.GetAxis("leftStickY")>-0.2 && Input.GetAxis("leftStickY")<0.2 ){
+				state = 1; // back to stand due to stick sitting in centre
+		}
+	}
+}
---
+++ Assets/Scripts/prey_Script.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var walking : boolean;
+
+
+function Start () {
+	walking = true;
+	
+}
+
+function Update () {
+	if (walking){
+		transform.Translate(Vector3.right * Time.deltaTime);
+	}
+//conditions for removing enemy from hierarchy
+	if (transform.position.x < -6 || transform.position.x > 6 ){
+		Destroy(gameObject);
+	} 
+
+}
